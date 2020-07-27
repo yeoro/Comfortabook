c0938425f13c504f193e12ac6c48af9a701aaaf2
엄강우 <yhnb33@gmail.com>
2020-07-27 14:14:48 +0900

    add init FE

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Grid from "@material-ui/core/Grid";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Grid container>
+        <Grid>hi</Grid>
+        <Grid>React</Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
