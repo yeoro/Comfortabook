ef103076193f27eb453c6c70509265033dac20a2
yeoro <dufgh1009@naver.com>
2020-07-30 20:08:06 +0900

    S03P12D204-310 [+] jpa test

--- backend/성열호/hellojpa/src/main/java/hellojpa/entity/Book.java
+++ backend/성열호/hellojpa/src/main/java/hellojpa/entity/Book.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,9 +15,11 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Entity
+@ToString(exclude = "member")
 public class Book {
 	
 	@Id 
@@ -38,8 +41,8 @@ public class Book {
 	
 	private String category;
 	
-	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "member_no")
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_no") // 양방향 연관관계의 주인만이 JoinCloumn, 외래키가 있는 곳을 주인으로
 	private Member member;
 	
 }
