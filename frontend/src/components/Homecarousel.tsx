bcd4bc02d49b2a23ae6f1020c87690c6e5253cfc
이지은 <ee2e@naver.com>
2020-07-31 17:03:16 +0900

    Edit home

--- frontend/src/components/Homecarousel.tsx
+++ frontend/src/components/Homecarousel.tsx
@@ -2,34 +2,42 @@ import React from "react";
 import Carousel from "nuka-carousel";
 
 export default class extends React.Component {
+  componentDidMount() {
+    this.setState({
+      mode: "Home",
+    });
+  }
+
   render() {
     return (
-      <Carousel>
-        <img
-          src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide1"
-          alt="slice1"
-        />
-        <img
-          src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide2"
-          alt="slice1"
-        />
-        <img
-          src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide3"
-          alt="slice1"
-        />
-        <img
-          src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide4"
-          alt="slice1"
-        />
-        <img
-          src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide5"
-          alt="slice1"
-        />
-        <img
-          src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide6"
-          alt="slice1"
-        />
-      </Carousel>
+      <div style={{ height: "300px" }}>
+        <Carousel>
+          <img
+            src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide1"
+            alt="slice1"
+          />
+          <img
+            src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide2"
+            alt="slice1"
+          />
+          <img
+            src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide3"
+            alt="slice1"
+          />
+          <img
+            src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide4"
+            alt="slice1"
+          />
+          <img
+            src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide5"
+            alt="slice1"
+          />
+          <img
+            src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide6"
+            alt="slice1"
+          />
+        </Carousel>
+      </div>
     );
   }
 }
