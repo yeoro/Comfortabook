faa911525b0d02dcd9f8cc6e955fe3a387afe5eb
yeoro <dufgh1009@naver.com>
2020-08-01 20:20:45 +0900

    도서 CRUD 구현

--- backend/ComfortaBook/src/main/java/com/gucci/cb/domain/Book.java
+++ backend/ComfortaBook/src/main/java/com/gucci/cb/domain/Book.java
@@ -7,11 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "book")
 public class Book {
 	
 	@Id
@@ -20,17 +27,37 @@ public class Book {
 	private Long bookNo;
 	
 	private String isbn;
+	
 	private String title;
 	
-	@Column(name = "CategoryName")
+	@Column(name = "category_name")
 	private String categoryName;
+	
 	private String author;
+	
 	private String publisher;
+	
 	private Date pubdate;
+	
 	private String description;
+	
 	private String cover;
 	
-	@Column(name = "member_no")
-	private String memberNo;
+//	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//	@JoinColumn(name = "member_no") // 양방향 연관관계의 주인만이 JoinCloumn, 외래키가 있는 곳을 주인으로
+//	private Member member;
+	
+	@Builder
+	public Book(String title, String author) {
+		this.author = author;
+		this.title = title;
+	}
+	
+	public void update(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+	
+	
 	
 }
