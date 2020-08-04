a30b723f10f9abcb6f0e301641b54a1cfe165b07
changhyun Kang <aaakch0316@gamil.com>
2020-08-04 10:57:25 +0900

    S03P13D204-78 [+] Predict in real-time

--- embedded/tensorflow/audio_recognition/index.js
+++ embedded/tensorflow/audio_recognition/index.js
@@ -4,6 +4,30 @@ const tf = require('@tensorflow/tfjs');
 // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
 require('@tensorflow/tfjs-node');
 
+
+let recognizer;
+
+function predictWord() {
+ // Array of words that the recognizer is trained to recognize.
+ const words = recognizer.wordLabels();
+ recognizer.listen(({scores}) => {
+   // Turn scores into a list of (score,word) pairs.
+   scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
+   // Find the most probable word.
+   scores.sort((s1, s2) => s2.score - s1.score);
+   document.querySelector('#console').textContent = scores[0].word;
+ }, {probabilityThreshold: 0.75});
+}
+
+async function app() {
+ recognizer = speechCommands.create('BROWSER_FFT');
+ await recognizer.ensureModelLoaded();
+ predictWord();
+}
+
+app();
+
+
 // Train a simple model:
 const model = tf.sequential();
 model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
