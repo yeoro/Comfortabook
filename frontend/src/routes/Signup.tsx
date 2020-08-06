bdb7194e2892db3d1aa948c7864dfee3b388d828
이지은 <ee2e@naver.com>
2020-08-06 15:36:25 +0900

    Edit signup

--- frontend/src/routes/Signup.tsx
+++ frontend/src/routes/Signup.tsx
@@ -71,33 +71,16 @@ function Loginpage() {
 
   const classes = useStyles();
 
-  // signup
-  // const signup = async (res: any) => {
-  //   const URL = ''
-  //   const signupResponse = await axios({
-  //     method: 'post',
-  //     url: URL,
-  //     data: {
-  //       email: JSON.stringify(signup.email),
-
-  //     }
-  //   })
-  // }
-
   // Kakao Login
   const KAKAO_API_KEY = "b4ce80d71e93a45b7b93c728c8193fa1";
 
   const success = async (res: any) => {
-    const URL = "/user/signup/kakao";
-    const kakaoLoginResponse = await axios({
-      method: "post",
-      url: URL,
-      data: {
-        accessToken: JSON.stringify(res.response.access_token),
-        name: JSON.stringify(res.profile.kakao_account.profile.nickname),
-      },
-      responseType: "json",
-    });
+    console.log(JSON.stringify({ accessToken: res.response.access_token }));
+    const URL = "http://i3d204.p.ssafy.io:9999/user/signup/kakao";
+    const kakaoLoginResponse = await axios.post(
+      URL,
+      JSON.stringify({ accessToken: res.response.access_token })
+    );
     console.log(kakaoLoginResponse);
   };
 
