fa789832a5bad92d1d105d047b3282dac4ee6c5d
엄강우 <yhnb33@gmail.com>
2020-08-10 12:49:04 +0900

    edit mypage_with_axios

--- frontend/src/components/Mypage.tsx
+++ frontend/src/components/Mypage.tsx
@@ -1,11 +1,44 @@
 import React from "react";
+import { Button, Grid } from "@material-ui/core";
+import { createStyles, WithStyles, withStyles } from "@material-ui/core/styles";
 
-function Mypage() {
-  return (
-    <div>
-      <h1>마이페이지</h1>
-    </div>
-  );
+const styles = () =>
+  createStyles({
+    root: {
+      padding: "5%",
+    },
+    btn: {
+      backgroundColor: "#f06292",
+      width: "100%",
+    },
+  });
+
+export interface Props extends WithStyles<typeof styles> {
+  detail: any;
 }
 
-export default Mypage;
+export interface State {
+  user_detail: any;
+}
+
+class Mypage extends React.Component<Props, State> {
+  state = {
+    user_detail: this.props.detail,
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid container className={classes.root}>
+        {/* <Grid item>
+          <p>email: {this.state.user_detail.email}</p>
+          <p>이름: {this.state.user_detail.name}</p>
+        </Grid> */}
+        <Grid container item>
+          <Button className={classes.btn}>로그아웃</Button>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(Mypage);
--- frontend/src/routes/Mainpage.tsx
+++ frontend/src/routes/Mainpage.tsx
@@ -7,12 +7,14 @@ import Home from "../components/Home";
 import Mypage from "../components/Mypage";
 import Library from "../components/Library";
 import "./Mainpage.css";
+// import axios from "axios";
 
 interface Props {
   modevalue: StatusTypes;
 }
 interface State {
   mode: StatusTypes;
+  user_detail: any;
 }
 
 type StatusTypes = "Home" | "Search" | "Library" | "Mypage";
@@ -20,12 +22,28 @@ type StatusTypes = "Home" | "Search" | "Library" | "Mypage";
 class Mainpage extends React.Component<Props, State> {
   state = {
     mode: this.props.modevalue,
+    user_detail: null,
   };
+
+  // loadDetail = async () => {
+  //   axios
+  //     .get("http://i3d204.p.ssafy.io:9999/user/detail")
+  //     .then(({ data }) => {
+  //       this.setState({
+  //         user_detail: data.Item,
+  //       });
+  //     })
+  //     .catch((e) => {
+  //       // API 호출이 실패한 경우
+  //       console.error(e); // 에러표시
+  //     });
+  // };
 
   componentDidMount() {
     this.setState({
       mode: "Home",
     });
+    // this.loadDetail();
   }
 
   getPage = () => {
@@ -36,7 +54,7 @@ class Mainpage extends React.Component<Props, State> {
     } else if (this.state.mode === "Library") {
       return <Library />;
     } else if (this.state.mode === "Mypage") {
-      return <Mypage />;
+      return <Mypage detail={this.state.user_detail} />;
     }
   };
 
