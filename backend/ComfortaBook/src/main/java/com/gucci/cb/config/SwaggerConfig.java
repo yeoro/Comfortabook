6fec8f27eadcd5af4f84a0633df3ef67f2015504
성열호 <dufgh1009@naver.com>
2020-08-15 19:54:13 +0000

    Update SwaggerConfig.java

--- backend/ComfortaBook/src/main/java/com/gucci/cb/config/SwaggerConfig.java
+++ backend/ComfortaBook/src/main/java/com/gucci/cb/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
 				.apiInfo(apiInfo())
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.gucci.cb.controller"))
-				.paths(PathSelectors.ant("/api/**"))
+				.paths(PathSelectors.ant("/**"))
 				.build();
 	}
 
