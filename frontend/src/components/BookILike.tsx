67aa2095f3496029bc634026f0dfd9329584e779
이지은 <ee2e@naver.com>
2020-08-17 17:37:30 +0900

    Edit bookilike

--- frontend/src/components/BookILike.tsx
+++ frontend/src/components/BookILike.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+import "./BookILike.css";
 
 interface Props {
   mybooks: any;
   recentBook: number;
+  gotoread: (bookNo: string, page: number) => void;
 }
 
 interface State {
@@ -10,12 +19,17 @@ interface State {
 }
 
 class BookILike extends React.Component<Props, State> {
-  constructor(props: any) {
+  constructor(props: Props) {
     super(props);
     this.state = {
       book: null,
     };
   }
+
+  read = () => {
+    this.props.gotoread(this.state.book.bookNo, this.state.book.page);
+  };
+
   readRecentBook = () => {
     this.props.mybooks.map((element: any) => {
       if (element.bookNo === this.props.recentBook) {
@@ -25,11 +39,36 @@ class BookILike extends React.Component<Props, State> {
       }
     });
   };
+
   getRecentBook = () => {
     if (!!this.state.book === true) {
-      return <h1>{this.state.book.title}</h1>;
+      return (
+        <div className="book-i-like">
+          <Card onClick={this.read}>
+            <CardActionArea>
+              <CardMedia
+                className="media"
+                image={this.state.book.cover}
+                title={this.state.book.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h3" component="h3">
+                  {this.state.book.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {this.state.book.description.substr(0, 130)} . . .
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
+      );
     } else {
-      return <h1>저장된 최근 도서가 없습니다.</h1>;
+      return (
+        <div className="book-i-like">
+          <h1>저장된 최근 도서가 없습니다.</h1>
+        </div>
+      );
     }
   };
   componentWillMount() {
